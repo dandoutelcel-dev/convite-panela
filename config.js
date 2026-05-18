32932375c44029365058001b3ab4e9813e14feb1
axiom <dandoutelcel@gmail.com>
2026-05-18 12:03:50 -0300

    Segurança API

--- config.js
+++ config.js
@@ -1,1 +1,9 @@
-
+export const firebaseConfig = {
+        apiKey: "AIzaSyBGlpqnTfsOc0n4vJDeOA2wcCel8bDBGyI",
+        authDomain: "panelaanaedani.firebaseapp.com",
+        databaseURL: "https://panelaanaedani-default-rtdb.firebaseio.com",
+        projectId: "panelaanaedani",
+        storageBucket: "panelaanaedani.firebasestorage.app",
+        messagingSenderId: "726922549771",
+        appId: "1:726922549771:web:10c943df7fbb0ac642850e"
+    };
