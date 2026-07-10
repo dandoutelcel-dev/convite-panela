07ced79fe33f3cc2d4fca42935a8cc0a89634132
axiom <dandoutelcel@gmail.com>
2026-07-10 16:00:55 -0300

    Add files via upload

---
+++ config_pad.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDJQsvYSUhDWEw8vpEemrqQXZu8anKeFUA",
+    authDomain: "padrinhos-c2754.firebaseapp.com",
+    databaseURL: "https://padrinhos-c2754-default-rtdb.firebaseio.com",
+    projectId: "padrinhos-c2754",
+    storageBucket: "padrinhos-c2754.firebasestorage.app",
+    messagingSenderId: "1002654987750",
+    appId: "1:1002654987750:web:60348e13c31d3de35a5227",
+    measurementId: "G-6RQ7568R4H"
+  };
